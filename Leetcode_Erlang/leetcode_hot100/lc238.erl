d07051d0015aac49e4ea90f0346ca023ecdaaaa9
xiaoyi <xiaoyi19991002@gmail.com>
2023-12-18 18:56:43 +0800

    lc 238 correct

--- Leetcode_Erlang/leetcode_hot100/lc238.erl
+++ Leetcode_Erlang/leetcode_hot100/lc238.erl
@@ -7,12 +7,24 @@
 -author("Administrator").
 
 %% API
--export([]).
+-export([test/0]).
+test() ->
+  product_except_self([1, 2, 3, 4]).
+
 -spec product_except_self(Nums :: [integer()]) -> [integer()].
 product_except_self(Nums) ->
-  SumMul = do(Nums, 1),
-  [ SumMul div  Num || Num <- Nums].
+  T1 = get_trig(Nums, [1]),
+  T2 = get_trig(lists:reverse(Nums), [1]),
+  do(T1, lists:reverse(T2), []).
 
-do([], Acc) -> Acc;
-do([H | Nums], Acc) ->
-  do(Nums, H * Acc).
+do([], [], Acc) ->
+  Acc;
+do([H1 | T1], [H2 | T2], Acc) ->
+  do(T1, T2, [H1 * H2 | Acc]).
+
+get_trig([_], Acc) ->
+  Acc;
+get_trig([Num | Nums], [A | Acc]) ->
+  get_trig(Nums, [Num * A, A | Acc]).
+
+
